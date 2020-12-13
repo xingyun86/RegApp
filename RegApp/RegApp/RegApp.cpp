e6d3d386810fec2dafddc48bf0f2b84c7efb37c6
xingyun86 <xingyun_ppshuai@yeah.net>
2020-12-14 00:08:30 +0800

    提交代码

---
+++ RegApp/RegApp/RegApp.cpp
@@ -0,0 +1,13 @@
+﻿// RegApp.cpp : Defines the entry point for the application.
+//
+
+#include "RegApp.h"
+
+int main(int argc, char ** argv)
+{
+	std::cout << "Hello CMake." << std::endl;
+	RegApp::Inst()->RegAuto(1);
+	//RegApp::Inst()->EnabledUAC(1);
+	system("PAUSE");
+	return 0;
+}
---
+++ RegApp/RegApp/RegApp.h
@@ -0,0 +1,50 @@
+﻿// RegApp.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+#include <windows.h>
+#include <winreg.h>
+
+class RegApp
+{
+public:
+  
+    LSTATUS EnabledUAC(BOOL bEnabled = FALSE)
+    {
+        HKEY hKey = NULL;
+        DWORD dwValue = bEnabled ? 1 : 0;
+        LSTATUS lStatus = ERROR_SUCCESS;
+        lStatus &= RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", 0, KEY_WRITE, &hKey);
+        dwValue = bEnabled ? 5 : 0;
+        lStatus &= RegSetValueExA(hKey, ("ConsentPromptBehaviorAdmin"), 0, REG_DWORD, (LPBYTE)&dwValue, sizeof(dwValue));
+        dwValue = bEnabled ? 1 : 0;
+        lStatus &= RegSetValueExA(hKey, ("EnableLUA"), 0, REG_DWORD, (LPBYTE)&dwValue, sizeof(dwValue));
+        dwValue = bEnabled ? 1 : 0;
+        lStatus &= RegSetValueExA(hKey, ("PromptOnSecureDesktop"), 0, REG_DWORD, (LPBYTE)&dwValue, sizeof(dwValue));
+        lStatus &= RegCloseKey(hKey);
+        return lStatus;
+    }
+
+    LSTATUS RegAuto(BOOL bAuto)
+    {
+        HKEY hKey = NULL;
+        DWORD dwValue = 0;
+        LSTATUS lStatus = ERROR_SUCCESS;
+        CHAR czFileName[MAX_PATH] = { 0 };
+        GetModuleFileNameA(NULL, czFileName, sizeof(czFileName) / sizeof(*czFileName));
+        lStatus &= RegOpenKeyExA(HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hKey);
+        lStatus &= RegSetValueExA(hKey, (strrchr(czFileName, '\\') + 1), 0, REG_SZ, (LPBYTE)czFileName, lstrlenA(czFileName) + 1);
+        lStatus &= RegCloseKey(hKey);
+        return lStatus;
+    }
+public:
+    static RegApp* Inst() {
+        static RegApp RegAppInstance;
+        return &RegAppInstance;
+    }
+};
